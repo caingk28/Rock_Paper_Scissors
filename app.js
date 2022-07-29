07e06bc0dde1457033a0e3c323aa62b507b5d1fa
caingk28 <k_gentle_king@hotmail.co.uk>
2022-07-29 18:52:55 +0100

    game working

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 
 
 // took a look at lots of different approahes for a random array //
-
-function computerChoice (){
-  const choices = ["Rock", "Paper", "Scissors"]
+const computerChoice = () => {
+  const choices = ['rock', 'paper', 'scissors']
   const randomNum = Math.floor(Math.random()*choices.length)
   const compChoice = choices[randomNum]
   return compChoice;
 }
+ 
+const playRound = (playerSelection, computerSelection) => {
 
-console.log(computerChoice())
-  
+   if (playerSelection === 'rock' && computerSelection === 'rock'){
+    return 'You tied, you both picked Rock'
+   } else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
+    return 'You tied, you both picked Scissors'
+   } else if (playerSelection === 'paper' && computerSelection === 'paper'){
+    return "You tied, you both picked Paper"
+  } else if (playerSelection === 'rock' && computerSelection === 'paper'){
+    return 'You lost, Paper covers Rock'
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+    return 'You won, Scissors cuts Paper'
+  } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+    return 'You won, Paper covers Rock'
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+    return 'You lost, Scuissors cuts Paper'
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+    return 'You won, Rock blunts Scissors'
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock"){
+    return 'You lost, Rock breaks Scissors';
+  }
+}
+
+const playerSelection = 'rock'
+const computerSelection = computerChoice()
+
+console.log(playRound(playerSelection, computerSelection))
+
+
+
 
