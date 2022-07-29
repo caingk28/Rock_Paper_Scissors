a907a0814cc7c9fe4d15b1417bdf86765cd0f8c0
caingk28 <k_gentle_king@hotmail.co.uk>
2022-07-29 16:57:37 +0100

    choice correct

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 
 
-
+// took a look at lots of different approahes for a random array //
 
 function computerChoice (){
-  const picks = ["Rock", "Paper", "Scissors"]
-  const randomNum = [Math.floor(Math.random()*picks.length)]
-  console.log(picks[randomNum])
+  const choices = ["Rock", "Paper", "Scissors"]
+  const randomNum = Math.floor(Math.random()*choices.length)
+  const compChoice = choices[randomNum]
+  return compChoice;
 }
 
-computerChoice()
+console.log(computerChoice())
   
 
