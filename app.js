32c392a4099054b9109263ab0c131654a0108007
caingk28 <k_gentle_king@hotmail.co.uk>
2022-07-30 23:28:13 +0100

    computer working

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+let playerScore = 0
+let comScore = 0
 
 // took a look at lots of different approahes for a random array //
 const computerChoice = () => {
@@ -17,25 +19,45 @@ const playRound = (playerSelection, computerSelection) => {
    } else if (playerSelection === 'paper' && computerSelection === 'paper'){
     return "You tied, you both picked Paper"
   } else if (playerSelection === 'rock' && computerSelection === 'paper'){
+    comScore++
     return 'You lost, Paper covers Rock'
   } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+    playerScore++
     return 'You won, Scissors cuts Paper'
   } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+    playerScore++
     return 'You won, Paper covers Rock'
   } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+    comScore++
     return 'You lost, Scuissors cuts Paper'
   } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+    playerScore++
     return 'You won, Rock blunts Scissors'
   } else if (playerSelection === "Scissors" && computerSelection === "Rock"){
+    comScore++
     return 'You lost, Rock breaks Scissors';
   }
 }
 
 const playerSelection = 'rock'
-const computerSelection = computerChoice()
+ 
 
-console.log(playRound(playerSelection, computerSelection))
+const  game = () => {
+  for (let i = 0; i < 5; i++) {
+    const computerSelection = computerChoice()
+    playRound(playerSelection, computerSelection)
+} 
 
+if (playerScore > comScore){
+  return 'Well done you beat the computer, what a genius!'
+} else if (playerScore < comScore) {
+  return 'Unlucky the master computer beat you! HAHAHA'
+} else {
+  return 'You tied with the evil computer. You need some practice!'
+}
 
+}
+
+console.log(game())
 
 
